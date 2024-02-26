9ce4fc6f6e0b09194957eeb23051b06e9e0ddc4e
UrkJuancho <juanvallejo7601@gmail.com>
2024-02-26 12:32:30 -0500

    formula medica, con funcion de añadir y eliminar elementos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'labs';
+
+  items: any[] = [];
+
+  constructor() {
+    this.agregarItem();
+  }
+
+  agregarItem() {
+    this.items.push({
+      Nombre: '',
+      Presentacion: '',
+      Cantidad: '',
+      Posologia: '',
+      Duracion: ''
+    });
+  }
+
+  eliminarItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  guardarDatos() {
+    // Aquí puedes enviar los datos al backend o realizar cualquier acción necesaria
+    console.log(this.items);
+  }
 }
