ceff1372e0ef41f6cb9e5f1db4898b9fd0565945
UrkJuancho <juanvallejo7601@gmail.com>
2024-03-15 10:40:35 -0500

    dont complete with other archives, this branch is the origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,54 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'labs';
-
-  items: any[] = [];
-
-  constructor() {
-    this.agregarItem();
-  }
-
-  agregarItem() {
-    this.items.push({
-      Nombre: '',
-      Presentacion: '',
-      Cantidad: '',
-      Posologia: '',
-      Duracion: ''
-    });
-  }
-
-  eliminarItem(index: number) {
-    this.items.splice(index, 1);
-  }
-
-  guardarDatos() {
-    // Aquí puedes enviar los datos al backend o realizar cualquier acción necesaria
-    console.log(this.items);
-  }
+  
 }
-
-
-// agregarItem() {
-  //   this.items.push({
-  //     Nombre: '',
-  //     Presentacion: '',
-  //     Cantidad: '',
-  //     Posologia: '',
-  //     Duracion: ''
-  //   });
-  // }
-
-  // eliminarItem(index: number) {
-  //   if (this.items.length > 1) {
-  //     this.items.splice(index, 1);
-  //   } else {
-  //     alert('Debe haber al menos un elemento.');
-  //   }
-  // }
-
-  // guardarDatos() {
-  //   // Aquí puedes enviar los datos al backend o realizar cualquier acción necesaria
-  //   console.log(this.items);
-  // }
