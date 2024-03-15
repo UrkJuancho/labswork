d39017f6b0c8dac1e84feecd271189aa0a1ba842
UrkJuancho <juanvallejo7601@gmail.com>
2024-03-15 10:18:45 -0500

    dropdown in revision

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
 import { SwitchPrevenComponent } from './switch-preven/switch-preven.component';
+import { DropdownMenuItemComponent } from './dropdown-menu-item/dropdown-menu-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleComponent,
-    SwitchPrevenComponent
+    SwitchPrevenComponent,
+    DropdownMenuItemComponent
   ],
   imports: [
     BrowserModule,
